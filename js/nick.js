dd389b2fb16abc5c8775679af61fc727517792ed
metrothepaster <102440825+metrothepaster@users.noreply.github.com>
2024-04-29 19:08:31 +0200

    Delete js directory

--- js/nick.js
+++
@@ -1,6 +0,0 @@
-function zapiszTekst() {
-    var tekst = document.getElementById("textInput").value;
-    localStorage.setItem("zapisanyTekst", tekst);
-    console.log("Zapisano tekst: " + tekst);
-
-}
