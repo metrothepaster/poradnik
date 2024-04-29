4cdaf38efe66cda8161915b8e2d1728a3ce074e1
metrothepaster <102440825+metrothepaster@users.noreply.github.com>
2024-04-29 16:28:49 +0200

    Update nick.js

--- js/nick.js
+++ js/nick.js
@@ -2,6 +2,5 @@ function zapiszTekst() {
     var tekst = document.getElementById("textInput").value;
     localStorage.setItem("zapisanyTekst", tekst);
     console.log("Zapisano tekst: " + tekst);
-    // Możesz dodać kod do przekierowania do innego pliku HTML tutaj, jeśli to konieczne
-    // np. window.location.href = "inne_plik.html";
+
 }
